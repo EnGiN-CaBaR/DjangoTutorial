9afb061f1937d78a121849e21619ad0d8ec18e05
engin <engin.c89@gmail.com>
2016-06-21 00:51:00 +0300

    fifth commit

--- polls/views.py
+++ polls/views.py
@@ -5,40 +5,62 @@ from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 
 
-def index(request):
-    print "Enter Index Function"
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    print latest_question_list
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    print "Exit Index Function"
-    return HttpResponse(template.render(context, request))
-    #return render(request, 'polls/index.html', context)
+# def index(request):
+#     print "Enter Index Function"
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     print latest_question_list
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     print "Exit Index Function"
+#     return HttpResponse(template.render(context, request))
+#     #return render(request, 'polls/index.html', context)
+#
+#
+# def detail(request, question_id):
+#     print "Enter Detail Function"
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#         print question
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     print "Exit Detail Function"
+#     return render(request, 'polls/detail.html', {'question': question})
+#     #question = get_object_or_404(Question, pk=question_id)
+#     #return render(request, 'polls/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     print "Enter Results Function"
+#     question = get_object_or_404(Question, pk=question_id)
+#     print "Exit Results Function"
+#     return render(request, 'polls/results.html', {'question': question})
 
 
-def detail(request, question_id):
-    print "Enter Detail Function"
-    try:
-        question = Question.objects.get(pk=question_id)
-        print question
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    print "Exit Detail Function"
-    return render(request, 'polls/detail.html', {'question': question})
-    #question = get_object_or_404(Question, pk=question_id)
-    #return render(request, 'polls/detail.html', {'question': question})
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    print "Enter Results Function"
-    question = get_object_or_404(Question, pk=question_id)
-    print "Exit Results Function"
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 
 def vote(request, question_id):
